197cda8c4584bdba531b847e76d3e92f2ba98bdb
Steven R <anguelr12@hotmail.com>
2020-07-22 12:40:43 -0500

    devops1 test

---
+++ src/main/java/co/com/strevens/devops/devops.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.strevens.devops;
+
+/**
+ *
+ * @author angue
+ */
+public class devops {
+    public static void main(String[] args) {
+        
+        System.out.println("Good");
+    }
+   
+    
+}
